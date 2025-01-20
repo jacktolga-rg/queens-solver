96ecd1e67bf7b4cd5728950495296d227242af26
Jack Rolf-Gökeş <jacktolga.rg@gmail.com>
2025-01-20 00:15:41 +0000

    Add Next.js config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/queens-solver",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
